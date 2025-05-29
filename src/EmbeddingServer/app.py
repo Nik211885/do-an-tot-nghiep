451907e024f4e45a503fa2b8b1aa2ff8ef15fd72
Lê Khắc Ninh <119054771+Nik211885@users.noreply.github.com>
2025-05-29 10:07:00 +0700

    simple flask

---
+++ src/EmbeddingServer/app.py
@@ -0,0 +1,12 @@
+﻿from flask import Flask
+from hello_word import say_hello
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return f"{say_hello()} my name is Ninh"
+
+if __name__ == '__main__':
+    app.run(debug=True)
